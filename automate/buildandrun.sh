800d3397a2fc78393871dacd6b84b9acef18b7c8
Raju George <rajugeorge@gmail.com>
2020-02-03 15:38:47 +0530

    Modified automate and script

--- automate/buildandrun.sh
+++ automate/buildandrun.sh
@@ -6,4 +6,7 @@ docker tag simple-site heppiedoc/simple-site
 
 docker push heppiedoc/simple-site
 
+docker container stop simple-site
+docker container rm simple-site
+
 docker run --name simple-site -d -p 8080:80 heppiedoc/simple-site
